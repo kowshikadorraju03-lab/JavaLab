088168d59299e560e72899f2a5216cf0c1fc468a
kowshika dorraju <kowshikadorraju03@gmail.com>
2026-07-24 23:42:20 +0530

    Add files via upload

---
+++ Task2_AccessAndPrintIndex.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Task2_AccessAndPrintIndex {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int index = 2;
+
+        System.out.println("Element at index " + index + " is: " + numbers[index]);
+    }
+}
