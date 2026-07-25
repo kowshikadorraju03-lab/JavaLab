87d77a2c7e582b684dd6c18ed0edd87a77550c59
kowshika dorraju <kowshikadorraju03@gmail.com>
2026-07-25 10:15:38 +0530

    Refactor digit sum operation to use user input

--- Task7_DigitSumOperation.java
+++ Task7_DigitSumOperation.java
@@ -1,26 +1,42 @@
 package lab1;
 
+import java.util.Scanner;
+
+@FunctionalInterface
+interface DigitSum {
+    int calculate(int num, String option);
+}
+
 public class Task7_DigitSumOperation {
 
     public static void main(String[] args) {
 
-        int number = 123456;
-        int evenSum = 0;
-        int oddSum = 0;
+        Scanner sc = new Scanner(System.in);
 
-        while (number > 0) {
-            int digit = number % 10;
+        System.out.print("Enter Number: ");
+        int num = sc.nextInt();
 
-            if (digit % 2 == 0) {
-                evenSum += digit;
-            } else {
-                oddSum += digit;
+        System.out.print("Enter Option (even/odd): ");
+        String option = sc.next();
+
+        DigitSum ds = (n, opt) -> {
+            int sum = 0;
+
+            while (n > 0) {
+                int digit = n % 10;
+
+                if (opt.equalsIgnoreCase("even") && digit % 2 == 0)
+                    sum += digit;
+                else if (opt.equalsIgnoreCase("odd") && digit % 2 != 0)
+                    sum += digit;
+
+                n /= 10;
             }
+            return sum;
+        };
 
-            number = number / 10;
-        }
+        System.out.println("Sum = " + ds.calculate(num, option));
 
-        System.out.println("Sum of Even Digits: " + evenSum);
-        System.out.println("Sum of Odd Digits: " + oddSum);
+        sc.close();
     }
 }
